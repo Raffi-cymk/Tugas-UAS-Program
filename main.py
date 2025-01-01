ffb2bd4bbd017e277aed8419a9a7455e478fc8a5
Raffi-cymk <raffipasha68@gmail.com>
2025-01-01 20:06:45 +0700

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-class Data:
-    def _init_(self):
-        self.data = []
-        
-    def add_record(self, record):
-        self.data.append(record)
-        
-    def get_all(self):
-        return self.data
